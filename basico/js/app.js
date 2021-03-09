a85935dbdb1d4b9bfa1c073aa1ce815922881331
Davi Bernardo <dnssites@gmail.com>
2021-03-09 15:31:25 -0300

    Js na pratica - Variaveis

--- basico/js/app.js
+++ basico/js/app.js
@@ -1,9 +1,24 @@
-//aula 1
-alert('Olá mundo!');//exibe alerta no navegador
-console.log('Olá mundo!');//exibe o conteudo no console F12, usado para debugs
+// ************ aula 1 *********************
+//alert('Olá mundo!');//exibe alerta no navegador
+//console.log('Olá mundo!');//exibe o conteudo no console F12, usado para debugs
 
-//aula 2
+// ************ aula 2 *********************
 //comentário 1 linha
 
 /*comentário multiplas linhas
   comentário em bloco*/
+
+// ************ aula 3 *********************
+//Variaveis guarda em memória dados que durande o processo vai mudando de valores
+var davi = 'Davi Bernardo'; //aparece n vezes no meu console
+console.log(davi); //string
+
+//podendo ser mudado o valor
+davi = true; //Boolean
+console.log(davi);
+
+davi = 19999; //int
+console.log(davi);
+
+davi = 3.14; //float
+console.log(davi);
