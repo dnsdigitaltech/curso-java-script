3633d6afd1c008f541bcc955bf8595df40bd553a
Davi Bernardo <dnssites@gmail.com>
2021-03-09 15:17:43 -0300

    Js na pratica - comentarios de codigo

--- basico/js/app.js
+++ basico/js/app.js
@@ -1,2 +1,9 @@
+//aula 1
 alert('Olá mundo!');//exibe alerta no navegador
 console.log('Olá mundo!');//exibe o conteudo no console F12, usado para debugs
+
+//aula 2
+//comentário 1 linha
+
+/*comentário multiplas linhas
+  comentário em bloco*/
