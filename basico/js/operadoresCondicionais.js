f0ecbdde1da3e7a80b0736aa92ace90f91ac96e3
Davi Bernardo <dnssites@gmail.com>
2021-03-11 17:57:02 -0300

    Operadores Condicionais IF e ELSE

---
+++ basico/js/operadoresCondicionais.js
@@ -0,0 +1,18 @@
+/******************** Operadores Condicionais IF e ELSE  ****************** */
+var number = true
+
+if(number)
+    console.log('É verdadeiro!') //É verdadeiro!
+
+if(2 == 2 && 3 === "3"){
+    console.log('****************')
+    console.log("2 é igual a 2")
+    console.log('****************')
+}else{
+    console.log('Não é igual')
+}
+
+if(4 === 4)
+    console.log("É igual")
+else    
+    console.log('É diferente')
