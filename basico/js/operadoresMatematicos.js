bf85e004d60fd5d63bc29c7a71329043f61ea1ca
Davi Bernardo <dnssites@gmail.com>
2021-03-11 16:19:13 -0300

    Operadores Aritméticos Ordem de Prioridades

--- basico/js/operadoresMatematicos.js
+++ basico/js/operadoresMatematicos.js
@@ -1,7 +1,7 @@
 /******************** Operadores Aritméticos ****************** */
-/*var n1 = 12
+var n1 = 12
 var n2 = 4
-
+/*
 //soma
  console.log(n1 + n2 + 4)
 
@@ -17,3 +17,21 @@ console.log(n1 / n2)
 //potencia
 console.log(n1 ** n2)*/
 
+/******************** Operadores Aritméticos Ordem de Prioridades  ****************** */
+/*console.log(2 + 4 - 3) //3
+console.log(4 - 3 + 2) //3
+console.log(4 - 3 + 2 * 2) //5
+console.log((4 - 3 + 2) * 2) //6
+console.log(4 - 3 + 2 / 2) //2
+console.log((6 - 2 + 2) / 2) //3
+console.log((6 - 2 + 2) / 2 + 2 * 3) //9
+console.log(((6 - 2 + 2) / 2 + 2) * 3) //15*/
+
+/******************** Operadores Aritméticos Incremento e Decremento  ****************** */
+console.log(n1++)//12
+console.log(n1)//13
+console.log(++n1)//14
+
+console.log(n1--)//14
+console.log(n1)//13
+console.log(--n1)//12
